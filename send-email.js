9710f8b9ba37f1c6836ae38b29f09e6290665888
youssieframadan <youssief.r@gmail.com>
2021-02-17 07:34:38 +0200

    a

--- send-email.js
+++ send-email.js
@@ -1,8 +1,13 @@
-var nodemailer = require('nodemailer');
+const sendmail = require('sendmail')({
+  devPort: 1025, // Default: False
+  devHost: 'localhost', // Default: localhost
+  smtpPort: 2525, // Default: 25
+  smtpHost: 'localhost' // Default: -1 - extra smtp host after resolveMX
+});
 const product = require('./models/product');
 
 async function sendEmail(){
-
+  console.log("sending email");
   products = await product.find({emailSent:false}).then((product)=>{
     var data=""
     product.forEach((item)=>{
@@ -13,29 +18,38 @@ async function sendEmail(){
       +"Product Link:"+'\n'+item["link"]+'\n'+'\n'
       
     })
-    console.log(data)
-    var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'machinerycollector087@gmail.com',
-      pass: 'nLI77&SFGObufUyDrZ'
-    }
-  });
+  //   var transporter = nodemailer.createTransport({
+  //   service: 'gmail',
+  //   auth: {
+  //     user: 'machinerycollector087@gmail.com',
+  //     pass: 'nLI77&SFGObufUyDrZ'
+  //   }
+  // });
 //Arabcorporationsupply@gmail.com
-  var mailOptions = {
-    from: 'machinerycollector087@gmail.com',
-    to: 'Arabcorporationsupply@gmail.com',
-    subject: 'machinery collector',
-    text: data,
-  };
+  // var mailOptions = {
+  //   from: 'machinerycollector087@gmail.com',
+  //   to: 'youssief.r@gmail.com',
+  //   subject: 'machinery collector',
+  //   text: data,
+  // };
 if(data!==""){
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('E-mail successfully sent: ' + info.response);
-    }
-  });
+  sendmail({
+    from: 'machinerycollector087@gmail.com',
+    to: 'youssief.r@gmail.com',
+    subject: 'machinery collector',
+    html: data,
+  }, function(err, reply) {
+    console.log(err && err.stack);
+    console.dir(reply);
+});
+
+  // transporter.sendMail(mailOptions, function(error, info){
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log('E-mail successfully sent: ' + info.response);
+  //   }
+  // });
 }
 })
   await product.updateMany({emailSent:false}, {"$set":{"emailSent": true}});
