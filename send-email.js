2939658f4f9a0267d5360ddbc03de533e2c1a48a
youssieframadan <youssief.r@gmail.com>
2021-02-28 11:40:04 +0200

    a

--- send-email.js
+++ send-email.js
@@ -23,7 +23,7 @@ async function sendEmail(){
 //Arabcorporationsupply@gmail.com,adamgces@gmail.com,khaled.mohamed.5811@gmail.com
   var mailOptions = {
     from: 'machinerycollector087@gmail.com',
-    to: 'youssief.r@gmail.com',
+    to: 'Arabcorporationsupply@gmail.com,adamgces@gmail.com,khaled.mohamed.5811@gmail.com,youssief.r@gmail.com',
     subject: 'machinery collector',
     text: data,
   };
